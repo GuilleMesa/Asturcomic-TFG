2ebe28908767c85b4b8c5bf631ec513a5b4a7287
GuilleMesa <uo278501@uniovi.es>
2026-04-12 17:23:32 +0200

    Archivos modificados para implementar Nginx

--- backend/index.js
+++ backend/index.js
@@ -5,25 +5,23 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Assets SVG 
+app.set("trust proxy", 1);
+app.use(express.json());
+
+// /assets para desarrollo local con Vite
 app.use("/assets", express.static(path.join(__dirname, "public", "assets")));
 
-// Frontend compilado 
-const appDist = path.join(__dirname, "public", "app");
-app.use(express.static(appDist));
-
 // API
-app.get("/api/health", (req, res) => res.send("OK"));
+app.get("/api/health", (_req, res) => {
+  res.send("OK");
+});
 
-app.get("/api/catalog", (req, res) => {
+app.get("/api/catalog", (_req, res) => {
   const catalogPath = path.join(__dirname, "catalog.json");
   const raw = fs.readFileSync(catalogPath, "utf-8");
   res.type("json").send(raw);
 });
 
-// Fallback SPA: cualquier ruta EXCEPTO /api y /assets
-app.get(/^(?!\/api\/|\/assets\/).*/, (req, res) => {
-  res.sendFile(path.join(appDist, "index.html"));
+app.listen(PORT, () => {
+  console.log(`Backend en http://localhost:${PORT}`);
 });
-
-app.listen(PORT, () => console.log(`Backend en http://localhost:${PORT}`));
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: "static",
+  },
   server: {
+    host: true,
     proxy: { // Con Vite establecemos proxy de api y assets para usar con React
       "/api": "http://localhost:8080",
       "/assets": "http://localhost:8080",
